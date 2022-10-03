50de87c06a0ed0f7dc826e0be680dfd284c48033
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2022-10-03 10:34:38 -0300

    login page and JS to handle login buttons

---
+++ login.js
@@ -0,0 +1,7 @@
+const login_form = document.querySelector('.login'); //form to login to the admin page
+
+// needs to validade login credentials. For now, it just redirects to products list page
+login_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.location = 'products.html';
+})
---
+++ script.js
@@ -0,0 +1,6 @@
+const login_btn = document.querySelector('.header__login'); //button to access login page
+
+// redirects to the login page
+login_btn.addEventListener('click', () => {
+    window.location = 'login.html';
+})
