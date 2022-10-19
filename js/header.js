d32a18a279385c629e59b666fe98faff3cd114a6
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2022-10-19 14:06:31 -0300

    page to show search results

--- js/header.js
+++ js/header.js
@@ -2,6 +2,13 @@ const searchIcon = document.querySelector('.header__search-icon');
 const searchBox = document.querySelector('.header__searchbox');
 const currentWindowWidth = window.innerWidth;
 
+// redirect to results page when Enter is pressed
+searchBox.addEventListener('keydown', e => {
+    if(e.keyCode === 13) {
+        window.location.href = `resultado.html?busca=${e.target.value}`;
+    }
+})
+
 // hide icon and show search box
 searchIcon.addEventListener('click', () => {
     searchBox.classList.add('header__searchbox--mobile');
@@ -17,7 +24,7 @@ searchIcon.addEventListener('click', () => {
  * a change in the height (changes in height don't affect the layout).
  * */ 
 onresize = () => {
-    // changes will be applied only if the window widht changed
+    // changes will be applied only if the window width changed
     if(currentWindowWidth !== window.innerWidth){
         searchBox.classList.remove('header__searchbox--mobile');
         if(window.innerWidth < 768){
